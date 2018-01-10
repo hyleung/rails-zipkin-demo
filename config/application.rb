3eff975cf7b1ac42f220e1568aa45f690e1cab7e
Ho Yan Leung <hy.leung@gmail.com>
2018-01-09 21:15:18 -0800

    Adds Zipkin tracer middleware

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'zipkin-tracer'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +22,13 @@ module RailsZipkinExample
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.use ZipkinTracer::RackHandler,
+                          {service_name:"rails-zipkin-demo",
+                           service_port:3000,
+                           json_api_host:'http://localhost:9411',
+                           sample_rate: 1,
+                           log_tracing: true
+                          }
   end
 end
